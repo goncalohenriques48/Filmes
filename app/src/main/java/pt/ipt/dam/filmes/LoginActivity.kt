5a4d035c3ae7a0ecd3c2002e5747e9b45b3b8988
Gonçalo Henriques <aluno21590@ipt.pt>
2024-10-26 22:43:15 +0100

    Login feito

--- app/src/main/java/pt/ipt/dam/filmes/LoginActivity.kt
+++ app/src/main/java/pt/ipt/dam/filmes/LoginActivity.kt
@@ -1,12 +1,21 @@
 package pt.ipt.dam.filmes
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+public class LoginActivity : AppCompatActivity() {
+    private lateinit var userEdt: EditText
+    private lateinit var passEdt: EditText
+    private lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,23 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initView();
+    }
+
+    private fun initView() {
+        userEdt = findViewById(R.id.editTextText);
+        passEdt = findViewById(R.id.editTextPassword);
+        loginBtn = findViewById(R.id.BT_Login);
+
+        loginBtn.setOnClickListener {
+            if (userEdt.text.toString().isEmpty() || passEdt.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please fill your user and password", Toast.LENGTH_SHORT).show()
+            } else if (userEdt.text.toString() == "test" && passEdt.text.toString() == "test") {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                Toast.makeText(this, "Your user and password is not correct", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
